6edf938077425cfc634edf32d895aa2a187ec49b
Christian Mourier <christian.mourier@gmail.com>
2016-12-18 03:38:32 +0100

    added enum class and project files

---
+++ GenericDataEntityHandler/GenericSQLEntityHandler/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSQLEntityHandler
+{
+    public enum SaveType
+    {
+        Insert,
+        Update,
+        InsertOrUpdate,
+        FastInsert
+    }
+}
